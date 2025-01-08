33a5a62531f1cd89fa3035eab0ad90ef7c29d7ca
CamiloSosaa <162145625+CamiloSosaa@users.noreply.github.com>
2025-01-08 10:12:04 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('CamiloSosaa/nodejsapp')
+            repositoryName('jcamilososa/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
